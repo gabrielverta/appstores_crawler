fd5fb0089f6318734e3bc7b278077ae973df5307
Gabriel Verta <gabrielverta@gmail.com>
2017-04-08 12:02:20 -0300

    - apple store: top apps inside category

--- parsers/apple.py
+++ parsers/apple.py
@@ -5,17 +5,18 @@ def categories(content):
     """
         Returns list of categories as follows:
             [
-                {'name': 'Category', 'url': 'http://url.com', 'children': [{'name': 'sub1', 'url': 'url1'}] 
+                {'name': 'Category', 'url': 'http://url.com', 'children': [{'name': 'sub1', 'url': 'url1'}]},
+                 ...
             ]
         
-        :param content: HTML of the page 
+        :param content: HTML of the page
         :return: list
     """
-    html = BeautifulSoup(content, 'html.parser')
-    nav = html.select("#genre-nav .grid3-column")[0]
-    items = nav.select('> .list > li')
-
     response = []
+
+    html = BeautifulSoup(content, 'html.parser')
+    items = html.select('#genre-nav .grid3-column > .list > li')
+
     for item in items:
         link = item.find('a')
         category = {'name': str(link.string), 'url': link['href'], 'children': []}
@@ -25,5 +26,32 @@ def categories(content):
             category['children'] = [{'name': str(c.string), 'url': c['href']} for c in sub_categories]
 
         response.append(category)
+
+    return response
+
+
+def top_apps_from_category(content):
+    """
+        Returns ordered list of top apps from category as follows:
+            [
+                {'name': 'First App', 'url': 'http://url1.com'},
+                {'name': 'Second App', 'url': 'http://url2.com'},
+                 ...
+            ]
+        
+        
+        :param content: HTML of the page 
+        :return: list
+    """
+    response = []
+
+    html = BeautifulSoup(content, 'html.parser')
+    items = html.select('#selectedgenre #selectedcontent ul li a')
+
+    for item in items:
+        response.append({
+            'name': str(item.string),
+            'url': item['href']
+        })
 
     return response
--- tests/parsers/test_apple.py
+++ tests/parsers/test_apple.py
@@ -6,13 +6,13 @@ TEMPLATES_PATH = os.path.join(os.path.dirname(os.path.realpath(os.path.dirname(_
 
 def test_parse_categories():
     """
-        Test that category list parser is working
-         
-        :return: 
+        Test that category list parser is working 
     """
     shopping = {'name': 'Shopping', 'url': 'https://itunes.apple.com/us/genre/ios-shopping/id6024?mt=8'}
+
     template = open(os.path.join(TEMPLATES_PATH, "categories.html")).read()
     categories = apple.categories(template)
+
     assert 25 == len(categories)
     assert shopping['name'] in [c['name'] for c in categories]
     assert shopping['url'] in [c['url'] for c in categories if c['name'] == shopping['name']]
@@ -21,15 +21,42 @@ def test_parse_categories():
 def test_parse_subcategories():
     """
         Test that subcategories are working
-        
-        :return: 
     """
     puzzle = {'name': 'Puzzle', 'url': 'https://itunes.apple.com/us/genre/ios-games-puzzle/id7012?mt=8'}
+
     template = open(os.path.join(TEMPLATES_PATH, "categories.html")).read()
     categories = apple.categories(template)
+
     names = [c['name'] for c in categories]
     assert 'Games' in names
     games = categories[names.index('Games')]
     assert 18 == len(games['children'])
     assert puzzle['name'] in [c['name'] for c in games['children']]
     assert puzzle['url'] in [c['url'] for c in games['children'] if c['name'] == puzzle['name']]
+
+
+def test_parse_top_by_category():
+    """
+        Test top apps inside a category
+    """
+    books = [{
+        'name': 'Kindle – Read eBooks, Magazines & Textbooks',
+        'url': 'https://itunes.apple.com/us/app/kindle-read-ebooks-magazines-textbooks/id302584613?mt=8'
+    }, {
+        'name': 'Audible – audio books, original series & podcasts',
+        'url': 'https://itunes.apple.com/us/app/audible-audio-books-original-series-podcasts/id379693831?mt=8',
+    }, {
+        'name': 'Wattpad - Read unlimited books and eBooks',
+        'url': 'https://itunes.apple.com/us/app/wattpad-read-unlimited-books-and-ebooks/id306310789?mt=8'
+    }]
+    top_count = 240
+
+    template = open(os.path.join(TEMPLATES_PATH, "single-category.html")).read()
+    top_book_apps = apple.top_apps_from_category(template)
+
+    assert top_count == len(top_book_apps)
+    for i in range(0, 3):
+        expected = books[i]
+        got = top_book_apps[i]
+        assert expected['name'] == got['name']
+        assert expected['url'] == got['url']
